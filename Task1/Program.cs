d4dc6da5644cc93fb0ca89ab37b0b559047fa0ef
Listratova Tatyana <listratova1@gmail.com>
2022-09-10 15:16:52 +0300

    added Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,31 @@
+﻿// Программа принимает на вход 5значное число и проверяет, является ли оно палиндромом
+
+System.Console.Write("Введите пятизначное число: ");
+
+try
+{
+    int num = Convert.ToInt32(Console.ReadLine());
+
+    if (num > 99999)
+    {
+        System.Console.WriteLine("Вы ввели не пятизначное число");
+    }
+    else
+    {
+        int numRevers = (num % 10 * 10000) + ((num / 10 % 10)*1000) + ((num / 100 % 10)*100) + ((num /1000 %10)*10) + num/10000;
+
+    if (num == numRevers)
+    {
+        System.Console.WriteLine("Введенное число является палиндромом");
+    }
+    else
+    {
+        System.Console.WriteLine("Введенное число НЕ палиндром!");
+    }
+    }    
+}
+catch
+{
+    System.Console.WriteLine("Вы ввели некорректные данные");
+}
+
