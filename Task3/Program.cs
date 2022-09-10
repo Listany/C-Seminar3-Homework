8b77cfc7f39ef8e9c79886a971aa38dbf77084ce
Listratova Tatyana <listratova1@gmail.com>
2022-09-10 18:41:38 +0300

    Added Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,24 @@
+﻿// принимает на вход число и выдает таблицу кубов чисел от 1 до N
+
+int [] GetNumCubes(int num)
+
+{
+	int[] numCubes = new int[num];
+
+	for (int i = 1; i <= num; i++)
+		{
+			numCubes[i-1] = i*i*i;			
+		}
+	return numCubes;
+}
+
+
+Console.WriteLine("Введите число N: ");
+int numN = int.Parse(Console.ReadLine());
+
+int[] results = GetNumCubes(numN);
+
+for (int i = 0; i < results.Length; i++)
+	{
+	Console.WriteLine(results[i]);
+	}
